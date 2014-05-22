d243ca40d065f55ec28dae36d1cc7bb4adbf41ca
Ariel Diamond <ariel.diamond@gmail.com>
2014-05-21 23:37:02 -0500

    user authentication

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,19 @@ get '/' do
   erb :index
 end
 
+post '/login' do
+	if User.authenticate(params[:username], params[:password])
+    @user = User.find_by_username(params[:username])
+    session[:user_id] = @user.id
+    redirect "/user/#{@user.id}"
+  else
+    @error = "Your username or password was incorrect."
+    erb :index
+  end
+end
+
+post '/signup' do
+	@user = User.create(params[:user])
+  redirect to "/" 
+end
+
